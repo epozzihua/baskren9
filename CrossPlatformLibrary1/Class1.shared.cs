c26811ce2285e249f519de27249b3d150b626b1c
epozzihua <qieyunpin3193@163.com>
2021-03-24 23:18:41 +0800

    Initial Commit

---
+++ CrossPlatformLibrary1/Class1.shared.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+namespace CrossPlatformLibrary1
+{
+    public partial class Class1 : ContentControl
+    {
+        public Class1()
+        {
+            Build();
+        }
+    }
+}
---
+++ CrossPlatformLibrary1/Class1.wasm.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+namespace CrossPlatformLibrary1
+{
+    public partial class Class1 : ContentControl
+    {
+        public void Build()
+        {
+            Content = new TextBlock { Text = "WASM" };
+        }
+    }
+}
